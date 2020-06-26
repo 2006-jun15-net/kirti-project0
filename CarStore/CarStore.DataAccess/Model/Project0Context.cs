affcc46df1b36d2bd870f08ac8740dee38afe76d
kirtipatel98 <kirti.bapa@gmail.com>
2020-06-26 15:36:49 -0700

    add database classes/property values

--- CarStore/CarStore.DataAccess/Model/Project0Context.cs
+++ CarStore/CarStore.DataAccess/Model/Project0Context.cs
@@ -22,15 +22,6 @@ namespace CarStore.DataAccess.Model
         public virtual DbSet<Sold> Sold { get; set; }
         public virtual DbSet<Stock> Stock { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:patel.database.windows.net,1433;Initial Catalog=Project0;Persist Security Info=False;User ID=kirti;Password=Radhupatel1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Customer>(entity =>
