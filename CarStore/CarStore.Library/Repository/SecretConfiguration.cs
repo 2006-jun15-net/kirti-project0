a9e4a03128670926c9758e6894d5bba8cc8600ec
kirtipatel98 <48999710+kirtipatel98@users.noreply.github.com>
2020-06-28 11:45:57 -0700

    Delete SecretConfiguration.cs

--- CarStore/CarStore.Library/Repository/SecretConfiguration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CarStore.Library.Repository
-{
-    internal class SecretConfiguration
-    {
-        internal const string ConnectionString = "Server=tcp:patel.database.windows.net,1433;Initial Catalog=Project0;Persist Security Info=False;User ID=kirti;Password=Swamishreeji1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
-    }
-}
