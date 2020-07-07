95e6cb59ef0704a644cc840a7eb2c3e1050254b4
Daniel Aasa <danielaasa42@gmail.com>
2020-07-07 12:29:12 -0700

    checking for change

--- CarStore/CarStore.Library/Model/Stock.cs
+++ CarStore/CarStore.Library/Model/Stock.cs
@@ -29,7 +29,7 @@ namespace CarStore.Library.Model
         }
 
         /// <summary>
-        /// Location ID
+        /// Location ID, run on change to check commit
         /// </summary>
         public int LocationId
         {
